e264e5e2fb3428cbafb2a8a89dd50f957d8b126e
dunkebiao <7765785>
2019-05-23 23:29:54 +0800

    init

--- app.py
+++ app.py
@@ -1,6 +1,43 @@
 # !/usr/bin/env python
 
 import pssh
+import getopt
+import signal
+import sys
 
-if __name__ == '__main__':
-    pssh.main()
+"""
+    This is main. you: nonsense.
+"""
+try:
+    options, args = getopt.getopt(
+        sys.argv[1:],
+        'hs:i:c:',
+        ['list', 'help']
+    )
+except getopt.GetoptError as e:
+    sys.exit(e.msg)
+
+pssh = pssh.Pssh()
+
+
+def set_win_size(signum, frame):
+    pssh.set_win_size()
+
+
+signal.signal(signal.SIGWINCH, set_win_size)
+
+operator = pssh.login
+host = []
+
+for name, value in options:
+    if name == '--list':
+        pssh.show_list()
+        exit()
+    if name in ('-h', '--help'):
+        print(pssh.help_str)
+        exit()
+
+if not host:
+    host = pssh.list()
+
+operator(host)
--- pssh/__init__.py
+++ pssh/__init__.py
@@ -6,9 +6,7 @@ __author__ = 'dunkebiao'
 
 import os
 import sys
-import getopt
 import pexpect
-import signal
 
 
 class Pssh:
@@ -89,40 +87,3 @@ Valid options:
                 continue
         self.set_win_size()
         process.interact()
-
-
-def main():
-    """
-        This is main. you: nonsense.
-    """
-    try:
-        options, args = getopt.getopt(
-            sys.argv[1:],
-            'hs:i:c:',
-            ['list', 'help']
-        )
-    except getopt.GetoptError as e:
-        sys.exit(e.msg)
-
-    pssh = Pssh()
-
-    def set_win_size(signum, frame):
-        pssh.set_win_size()
-
-    signal.signal(signal.SIGWINCH, set_win_size)
-
-    operator = pssh.login
-    host = []
-
-    for name, value in options:
-        if name == '--list':
-            pssh.show_list()
-            exit()
-        if name in ('-h', '--help'):
-            print(pssh.help_str)
-            exit()
-
-    if not host:
-        host = pssh.list()
-
-    operator(host)
